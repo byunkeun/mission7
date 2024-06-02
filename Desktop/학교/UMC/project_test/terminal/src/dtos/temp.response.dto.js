b6d94e887941c917290e44acd940658c3ec54cb2
byunkeun <byunkeun26@gmail.com>
2024-06-02 20:47:20 +0900

    t

---
+++ Desktop/학교/UMC/project_test/terminal/src/dtos/temp.response.dto.js
@@ -0,0 +1,12 @@
+// temp.response.dto.js
+
+
+export const tempResponseDTO = (data) => {
+    return {"testString" : data};
+}
+
+// temp.response.dto.js
+
+export const flagResponseDTO = (flag) => {
+    return {"flag" : flag};
+}
